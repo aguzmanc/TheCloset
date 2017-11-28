432a4faa6ede5c894563a0bd85a45e444b9d4911
VG <vengadoravg@gmail.com>
2017-11-28 11:04:36 -0400

    less tight treshold for the key

--- TheCloset/Assets/Items/Key and Lock/Key.cs
+++ TheCloset/Assets/Items/Key and Lock/Key.cs
@@ -36,8 +36,8 @@ namespace Vg {
             _forcedDrop = false;
 
             if (!_forcedDrop &&
-                Vector3.Angle(transform.forward, OwnerLock.EnterAngle.forward) +
-                Vector3.Angle(transform.up, OwnerLock.EnterAngle.up) > Tresholds.Rotation) {
+                (Vector3.Angle(transform.forward, OwnerLock.EnterAngle.forward) > Tresholds.Rotation ||
+                 Vector3.Angle(transform.up, OwnerLock.EnterAngle.up) > 80)) {
                 ForceDrop();
             }
         }
--- TheCloset/Assets/Scripts/Tresholds.cs
+++ TheCloset/Assets/Scripts/Tresholds.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
 
 public class Tresholds {
-    public static float Rotation = 20; // º
+    public static float Rotation = 30; // º
 }
