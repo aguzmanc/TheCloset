576ac927742ecbea2db739e01eded38b9a011a81
MONOLYTH\arn <arnold@toptal.com>
2017-11-28 10:19:17 -0400

    Closet Inmersivo, luz, tele y noticias

---
+++ TheCloset/Assets/TVLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVLight : MonoBehaviour 
+{
+	public float Velocity=1;
+	public float MaxIntensity = 6;
+	public float MinIntensity = 5;
+
+	float t;
+	Light _light;
+
+	void Start () 
+	{
+		t = 0;
+		_light = GetComponent<Light> ();
+	}
+	
+	void Update () 
+	{
+		float p = Mathf.PerlinNoise (t, 0);	
+		t += Time.deltaTime * Velocity;
+
+		_light.intensity = MinIntensity + (MaxIntensity-MinIntensity) * p;
+	}
+}
---
+++ TheCloset/Assets/VRListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRListener : MonoBehaviour 
+{
+	AudioSource _src;
+
+	public float MaxDistance;
+	public float MaxVolume;
+
+	void Start () 
+	{
+		_src = GameObject.FindObjectOfType<AudioSource> ();
+		
+	}
+
+	public float dist;
+	public float p;
+
+	void Update () 
+	{
+		_src.panStereo = -1f * Vector3.Dot (transform.right, Vector3.forward);
+
+		dist = (transform.position - _src.transform.position).z;
+
+		float p = 1f - dist / MaxDistance;
+
+		_src.volume = p * MaxVolume;
+	}
+}
